367576bdad459e5856db3045428d636699e7c8a3
Devohka <110058157+Devohka@users.noreply.github.com>
2023-04-14 17:44:15 +0300

    js-4

---
+++ js/14-04.js
@@ -0,0 +1,27 @@
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+const totalPrice = pricePerItem * orderedQuantity;
+console.log(totalPrice)
+
+const age = "17";
+const passed =
+    age === "18" || age !=="17";
+console.log(passed);
+
+const savedPassword = "qwrt12345";
+const password = "qwrt12346";
+const isMatch = savedPassword === password;
+console.log(isMatch);
+
+let a = 5;
+a += 7;
+console.log(a);
+let b = 10;
+b -= 6;
+console.log(b);
+let c = 15;
+c *= 45;
+console.log(c);
+let d = 20;
+d /= 2;
+console.log(d);
